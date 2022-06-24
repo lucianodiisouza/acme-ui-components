c089a62c147126285f9be2157c4a1113898eecb4
Luciano dii Souza <lucianodiisouza@hotmail.com>
2022-06-24 01:18:33 -0300

    feat: initial commit

---
+++ src/Button.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Button = (props) => {
+  const { type } = props
+
+  return (
+    <button type={type || 'submit'} className='acme-button'>
+      {props.children}
+    </button>
+  )
+}
+
+export default Button
